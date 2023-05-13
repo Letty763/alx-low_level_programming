d161b721b8fdff3f89ca4613b3757f17b6d7582f
Letty Makore <lettymakore1@gmail.com>
2023-05-13 03:43:00 +0200

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,23 +9,24 @@
 * Return: this is the actual number of letters it could read and print,
 * Provided that the file can not be opened or read, return 0,
 * Provided that filename is NULL return 0,
-* In case write fails or does not write the expected amount of bytes, return 0
+* In case write fails or does not write the expected amount of bytes,return {0}
 */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buf;
-ssize_t fd;
-ssize_t w;
-ssize_t t;
- 
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-buf = malloc(sizeof(char) * letters);
-t = read(fd, buf, letters);
-w = write(STDOUT_FILENO, buf, t);
- 
-free(buf);
-close(fd);
-return (w);
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
 }
